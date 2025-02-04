73636e4f8348605aa0c79bd2463890da8018f294
Minase Taye <minasegirma5@gmail.com>
2025-02-04 21:48:22 +0300

    DBT model

---
+++ ethiomedi/models/example/business_transformed.sql
@@ -0,0 +1,51 @@
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+    -- Select the columns you need, excluding 'channel_title'
+    SELECT DISTINCT
+        id,
+        REPLACE(channel_username, '@', '') AS channel_username,  -- Remove '@' from channel_username
+        message,
+        DATE(message_date) AS date  -- Extract only year, month, and day from date
+    FROM {{ source('public', 'telegram_messages') }} -- Reference the source table
+),
+
+-- Filter only data related to 'lobelia4cosmetics'
+lobelia_data AS (
+    SELECT 
+        id,
+        channel_username,
+        message,
+        date
+    FROM source_data
+    WHERE channel_username = 'lobelia4cosmetics'
+),
+
+-- Extract product, price, and address from the message column
+business_data AS (
+    SELECT 
+        id,
+        channel_username,
+        date,
+        -- Extract product
+        SUBSTRING(message FROM '^(.*?)(\n|$)') As product,
+        -- Extract price 
+        SUBSTRING(message FROM 'Price\s*([0-9]+(?:,?[0-9]*)(?:\.\d+)?)') AS price,
+        -- Extract address 
+        SUBSTRING(message FROM '(?:Adress|Address):-\s*(.+?school)') AS address
+    FROM lobelia_data
+
+)
+
+-- Final selection of transformed data, dropping the 'message' column
+SELECT 
+    id,
+    channel_username,
+    date,
+    product,
+    price,
+    address
+FROM business_data
+
+WHERE 
+    price IS NOT NULL
